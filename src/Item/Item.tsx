974756ac78d1aa27fa7bc31c27ab184a5639bc6e
Mustafa Mwafy <mwafy092@gmail.com>
2021-03-16 13:21:26 +0200

    add item component

---
+++ src/Item/Item.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { CartItemsTypes } from '../App';
+import { Wrapper } from './Item.styles';
+
+type Props = {
+    item: CartItemsTypes;
+    handleAddToCart: (clickedItem: CartItemsTypes) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+        </div>
+        <Button onClick={() => handleAddToCart(item)}>Add to Cart</Button>
+    </Wrapper>
+);
