0dda9de00b41aa1251e5e6a94af68a078fe39e4d
Mustafa Mwafy <mwafy092@gmail.com>
2021-03-16 14:18:01 +0200

    finish cart component

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -7,6 +7,34 @@ type Props = {
     addToCart: (clickedItem: CartItemsTypes) => void;
     removeFromCart: (id: number) => void;
 };
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>;
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className='info'>
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className='buttons'>
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => removeFromCart(item.id)}>
+                    -
+                </Button>
+                <p>{item.amount}</p>
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => addToCart(item)}>
+                    +
+                </Button>
+            </div>
+        </div>
+        <img src={item.image} alt={item.title} />
+    </Wrapper>
+);
 
 export default CartItem;
