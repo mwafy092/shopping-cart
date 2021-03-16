144aea0414cb02d0c39201fded5da3363cf43449
Mustafa Mwafy <mwafy092@gmail.com>
2021-03-16 13:12:13 +0200

    react query used

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import { Wrapper } from './App.styles';
 // components
 
-const getProducts = () => {
-    fetch('https://fakestoreapi.com/products')
-        .then((response) => response.json())
-        .then((products) => products);
+// type interface
+export type CartItemsTypes = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
 };
-getProducts();
+
+const getProducts = async (): Promise<CartItemsTypes[]> => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    return data;
+};
+
 const App = () => {
+    const { data, isLoading, error } = useQuery<CartItemsTypes[]>(
+        'products',
+        getProducts
+    );
+    console.log('data ', data);
+
+    if (isLoading) return <CircularProgress />;
+    if (error) return <div>Something is going wrong...</div>;
     return (
         <div className='App'>
             <h1>hello</h1>
