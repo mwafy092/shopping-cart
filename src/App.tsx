37963317d260e498010c604f5a15c698f1d1c968
Mustafa Mwafy <mwafy092@gmail.com>
2021-03-16 12:49:13 +0200

    api setup for fake products

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,15 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
+import { Wrapper } from './App.styles';
 // components
 
+const getProducts = () => {
+    fetch('https://fakestoreapi.com/products')
+        .then((response) => response.json())
+        .then((products) => products);
+};
+getProducts();
 const App = () => {
     return (
         <div className='App'>
